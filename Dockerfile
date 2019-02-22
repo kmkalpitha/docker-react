68c4471e632662c2cbe3e30e2e05423f5bdd24f5
Kalpitha KM <kalpitha@sparshik.com>
2019-02-22 15:55:42 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Specify a base image
+FROM node:alpine as builder
+
+WORKDIR '/app'
+
+# Install some dependencies
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Specify nginx
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
+
---
+++ build/precache-manifest.f9c7601dfd39e3c3ddd83acd25a7868e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f0f5515d2db0363b5bc8",
+    "url": "/static/js/main.f0f5515d.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "f0f5515d2db0363b5bc8",
+    "url": "/static/css/main.770c172f.chunk.css"
+  },
+  {
+    "revision": "7093320591fdacbb8a14d5b9822ae81d",
+    "url": "/index.html"
+  }
+];
